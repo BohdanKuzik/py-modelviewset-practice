cb4f87194d2d534e0054e289eeb562c83c695c76
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-08-18 20:50:32 +0200

    Solution

--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,15 @@
-# Create your urls here
+from rest_framework import routers
 
-urlpatterns = []
+from author.views import AuthorViewSet
+
+
+router = routers.DefaultRouter()
+router.register(
+    "manage",
+    AuthorViewSet,
+    basename="manage"
+)
+
+urlpatterns = router.urls
+
+app_name = "author"
